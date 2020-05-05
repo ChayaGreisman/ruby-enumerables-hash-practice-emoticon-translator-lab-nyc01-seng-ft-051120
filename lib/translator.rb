9c1de2c0969016a874296a81431a94d1e29cdf47
Chaya Greisman <chayamgreisman@gmail.com>
2020-05-05 20:17:21 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
-# require modules here
 
-def load_library
-  # code goes here
+require 'yaml'
+
+def load_library(file)
+  
+  hash_of_emoticons=YAML.load_file(file)
+  
+  hash_of_emoticons.each |name_of_emoticon, emojis| do
+    emojis.each |emoj| do
+  
+  hash_of_emoticons[:name_of_emoticon]={}
+  hash_of_emoticons[:name_of_emoticon][:english]=emoj[0] 
+  hash_of_emoticons[:name_of_emoticon][:japanese]=emoj[1]
+ 
+    end
+  end
+  hash_of_emoticons
 end
+
+
 
 def get_japanese_emoticon
+  
+  load_library()
   # code goes here
 end
 
+
+
 def get_english_meaning
+  
+  load_library()
   # code goes here
+  
 end
